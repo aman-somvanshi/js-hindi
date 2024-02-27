160191dc67476d8e847de8b18fa89cc186d0068f
aman-somvanshi <amansomvanshi2002@gmail.com>
2024-02-27 18:05:35 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)// we are using node js not browser
+
+console.log(3 + 3); //code readability should be high
+
+console.log("Aman");
+
+let name = "Aman";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+//  PRIMITIVE DATA TYPE
+
+//number => 2 to the power of 53 is the range
+//bigint
+// string
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol -m for finding uniqueness in react
+
+//object
+
+console.log(typeof "Aman");
+console.log(typeof age);
+console.log(typeof null); //gives object as output
+console.log(typeof undefined); //gives undefined as output

523fd71d6d91206bf6585a78181b9fc9adc71fe9
aman-somvanshi <amansomvanshi2002@gmail.com>
2024-03-05 20:50:45 +0530

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+// While loop
+let index=0;
+while (index<=10) {
+    // console.log(`Value of ines is ${index}`);
+    index=index+2;
+}
+
+let myArray=[`Superman`, `Batman`, `IshPiderman`]
+let arr=0;
+while (arr<myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr=arr+1;
+}
+
+// +++++++++++++Do While loop++++++++++++++++++++
+//Kaam pehle hoga , condition baad me check hogi
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score<=10);

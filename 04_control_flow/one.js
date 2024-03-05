3cfaf6ca1ae023a75476a862eb8e3fbdf54728a6
aman-somvanshi <amansomvanshi2002@gmail.com>
2024-03-05 08:54:48 +0530

    if and switch

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,9 +1,9 @@
 // if
 
-const isUserLoggedIn=true
-const temperature = 41
-if(2 === "2"){
-    console.log("executed");
+const isUserLoggedIn = true;
+const temperature = 41;
+if (2 === "2") {
+  console.log("executed");
 }
 
 // Comparison Operators -- <, >, <=, >=, ==, !=, === (Strict equal checks datatype and value both)
@@ -12,11 +12,46 @@ if(2 === "2"){
 // 5 !== 5 // false, because both are the number 5
 // 2 !==6
 
-if(temperature === 40 ){
-    console.log("Temperature is less than 50");
-}
-else {
-    console.log("temperature is greater than 50");
-}
-console.log("This line is Always executed")  
+// if(temperature === 40 ){
+//     console.log("Temperature is less than 50");
+// }
+// else {
+//     console.log("temperature is greater than 50");
+// }
+// console.log("This line is Always executed")
 // Assignment Operator -- =
+
+// const score = 200
+// if(score>100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+
+// }
+
+// console.log(`User power: ${power}`);
+
+// const balance = 1000
+// if (balance > 500)
+// console.log(`test`),   //not recommended
+// console.log(`test2`);
+
+// if (balance < 500) {
+//     console.log(`less than 500`);
+// } else if(balance < 750){
+//     console.log(`less than 750`);
+// } else if(balance < 900){
+//     console.log(`less than 900`);
+// } else {
+//     console.log(`less than 1200`);
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+if (userLoggedIn && debitCard && 2 == 3) {
+  console.log(`Allow to buy course`);
+}
+if (loggedInFromEmail || loggedInFromGoogle) {
+  console.log(`Allow to buy course`);
+}

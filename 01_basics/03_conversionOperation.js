392e0b0e49e26114958a11a91a591801e002ce26
aman-somvanshi <amansomvanshi2002@gmail.com>
2024-03-13 00:09:48 +0530

    added the outputs

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,26 +1,27 @@
 let score = "33abc";
 
-console.log(typeof score);
+console.log(typeof score); //Output-string
 console.log(typeof score);
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+console.log(typeof valueInNumber);//Output - number
+console.log(valueInNumber); //Output- NaN
 
+// let score1=Number(null)
+// console.log(score1);  //Output - 0
 let score1 = null;
-//let score1=Number(null)
-console.log(score1);
+console.log(score1); null
 
 //let score2=undefined
 let score2 = Number(undefined);
-console.log(typeof score2);
-console.log(score2);
+console.log(typeof score2); //output-number
+console.log(score2); //output- NaN
 
 // let score4=true
 let score4 = true;
 let valueInNumber1 = Number(score4);
-console.log(typeof valueInNumber1);
-console.log(valueInNumber1);
+console.log(typeof valueInNumber1); // OUTPUT-NUMBER
+console.log(valueInNumber1);// OUTPUT - 1
 
 // "33" => 33
 // "33abc" => NaN
@@ -29,7 +30,7 @@ console.log(valueInNumber1);
 let isLoggedIn = "abc";
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+console.log(booleanIsLoggedIn); // output - true
 
 
 // 1=> true
@@ -39,5 +40,5 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+console.log(stringNumber); //output- 33
+console.log(typeof stringNumber); //output - string

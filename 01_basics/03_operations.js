ea24b03a0ccee41f86a0a071d667b4ed5727a988
aman-somvanshi <amansomvanshi2002@gmail.com>
2024-03-13 00:32:06 +0530

    added outputs

--- 01_basics/03_operations.js
+++ 01_basics/03_operations.js
@@ -6,7 +6,7 @@ let negValue = -value;
 // console.log(2-2);
 // console.log(2*2);
 // console.log(2**3);
-// console.log(2/3);
+// console.log(2/3); // Output - 0.6666666666666
 // console.log(2%3);
 
 let str1 = "hello";
@@ -15,27 +15,29 @@ let str2 = " Aman";
 let str3 = str1 + str2;
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2" + 2);
-// console.log("1" + 2 + 2);
+// console.log("1" + 2); //Output -12
+// console.log(1 + "2" + 2); //Output - 122
+// console.log("1" + 2 + 2); //Output - 122
+// console.log(1 + 2 + "2"); //Output - 32
 
 //If string is first , then all the operands are treated as string
 //If string is at last, then the first operands are calculated as they are
 
-// console.log(1 + 2 + "2");
 // console.log(3+4*5%3);
 
 // Above ways are not appreciated in the corporate
 
-// console.log(+true);
-// console.log(+"");
+// console.log(+true); //Output -1
+// console.log(+"");//Output - 0
 
 let num1, num2, num3;
 num1 = num2 = num3 = 2 + 2;//not appreciated
 
 let gameCounter = 100
-++gameCounter
-console.log(gameCounter); 
+// const newvalue = gameCounter++
+// console.log(newvalue); //Output - 100
+const newvalue = ++gameCounter
+console.log(newvalue); //Output - 101
 
 // If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
 

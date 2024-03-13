475f0ae080366158f79f0a40b780b2384f0331f4
aman-somvanshi <amansomvanshi2002@gmail.com>
2024-03-13 20:09:51 +0530

    Outputs added

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -11,9 +11,10 @@ const isLoggedIn = false;
 const outsideTemp = null; // null is not 0
 let userEmail; // the value of this variable is undefined
 const id = Symbol("123");
+console.log(id);//Symbol(123)
 const anotherId = Symbol("123");
 
-// console.log(id === anotherId);
+console.log(id === anotherId);//false
 
 const bigNumber = 345678687436648754879n;
 
@@ -28,14 +29,15 @@ let myObj = {
 };
 
 const myFunction = function () {
-  //console.log("Hello World");
+  console.log("Hello World");
 };
 
 
-// console.log(typeof outsideTemp); //note that null gives object as datatype
-// console.log(typeof NaN);   // note that NaN returns number as datatype
+console.log(typeof outsideTemp); //note that null gives object as datatype
+console.log(typeof NaN);   // note that NaN returns number as datatype
+console.log(typeof undefined);//note that undefined returns undefined as datatype
 
-//console.log(typeof myFunction);
+console.log(typeof myFunction);//function
 
 
 
@@ -50,22 +52,22 @@ let myName="Aman"
 
 let anotherName=myName
 anotherName="C3PO"
-console.log(anotherName);
-console.log(myName);
+console.log(anotherName);//C3PO
+console.log(myName);//Aman
 
-// In above cases copies of the original value that is, of myName is being given to another Name
+// In above cases copies of the original value that is, of the variable myName is being given to the variable anotherName
 
-//UserOne is stored in Heap
+//UserOne is stored in Heap as it is an object
 let userOne={
     email:"ab@google.com",
     upi:"user@ybl"
-
 }
 
 let userTwo = userOne  // Here both the variables are being referenced to the same location
 
 userTwo.email="aman@google.com"
-console.log(userOne);
-console.log(userTwo);
+console.log(userOne);//{ email: 'aman@google.com', upi: 'user@ybl' }
+console.log(userTwo);//{ email: 'aman@google.com', upi: 'user@ybl' }
 
+//notice that on changing the value of email key of userTwo object, the value of email key of userOne also changes as both the objects were pointing to the same location in the heap
 // above signifies that the value is changed in both the objects

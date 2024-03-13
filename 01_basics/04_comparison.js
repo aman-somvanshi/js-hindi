cda64a519e7516a40f21e3f5fa00360849c578c7
aman-somvanshi <amansomvanshi2002@gmail.com>
2024-03-13 18:00:21 +0530

    Outputs added

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -5,19 +5,20 @@
 // console.log(2!=1);
 
 
-// console.log("2">1);
-// console.log("02">1);
+// console.log("2">1); //output - true
+// console.log("02">1); //output - true
 
-// console.log(null>0);
-// console.log(null==0);
-// console.log(null>=0);
+// console.log(null>0); // output - false
+// console.log(null==0);// output - false
+// console.log(null>=0);// output - true
 
-// console.log(undefined==0);
-// console.log(undefined>0);
-// console.log(undefined<0);
+// console.log(undefined==0); //false
+// console.log(undefined>0); //false
+// console.log(undefined<0); //false
+// console.log(undefined == null); //true
 
 // Above three paragraphs type of conversions should be avoided
 
 // === strict check --- checks value and datatype
 
-console.log("2"===2);
+console.log("2"===2); //false
